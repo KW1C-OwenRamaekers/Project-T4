1e39d0a89e68512546eb60d88041598b4d1d5ccf
owen-the-kid <owenramaekers2006@gmail.com>
2025-05-22 10:57:26 +0200

    basic

---
+++ Includes/nav.php
@@ -0,0 +1,7 @@
+    <nav>
+        <h1>Cooking.com</h1>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../Search.php">Search</a></li>
+        </ul>
+    </nav>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cooking.com</title>
+</head>
+<body>
+    <?php include 'Includes/nav.php'; ?>
+</body>
+</html>
