59964bd14bf5a4795267a35f05d6581a9b34dc6b
owen-the-kid <owenramaekers2006@gmail.com>
2025-05-22 12:21:02 +0200

    css stuff

--- Includes/nav.php
+++ Includes/nav.php
@@ -1,6 +1,6 @@
     <nav>
-        <h1>Cooking.com</h1>
-        <ul>
+        <ul id="nav-top">
+            <li><h1>Cooking.com</h1></li>
             <li><a href="../index.php">Home</a></li>
             <li><a href="../Search.php">Search</a></li>
         </ul>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cooking.com</title>
+    <link rel="stylesheet" href="Styles/Core.css">
+    <link rel="stylesheet" href="Styles/index.css">
 </head>
 <body>
     <?php include 'Includes/nav.php'; ?>
