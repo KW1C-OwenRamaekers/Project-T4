5f548441dd8e333de626d8352c84fd79e1603fc6
owen-the-kid <owenramaekers2006@gmail.com>
2025-06-18 09:58:44 +0200

    footer styling

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,3 +1,8 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Owen Ramaekers</p>
+<footer id="footer">
+    <p class="time"><?php echo "De huidige tijd is: " . date("H:i:s"); ?></p>    
+    <?php if (isset($_SESSION['username'])): ?>
+        <p class="user">Gebruiker: <?= $_SESSION['username'] ?></p>
+    <?php endif; ?>
+    <p class="copyright">&copy; <?php echo date('Y'); ?> Owen Ramaekers</p>
 </footer>
+
