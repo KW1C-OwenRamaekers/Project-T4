c86df096513b85dccc3979504065d03af32067a3
owen-the-kid <owenramaekers2006@gmail.com>
2025-06-18 10:28:07 +0200

    account styling

--- pages/Account.php
+++ pages/Account.php
@@ -15,9 +15,26 @@ Creator: Owen Ramaekers
 <body>
     <?php
     include '../Includes/nav.php';
-    echo '<h1>Welkom, ' . $_SESSION['username'] . '</h1>';
+    echo '<h1 id="welcome">Welkom, ' . $_SESSION['username'] . '</h1>';
     ?>
     <button><a href="create_post.php">Post een recept</a></button>
+    <h2 id="your-posts">Je posts:</h2>
+    <ul class="horizontal-list">
+        <?php
+        $db = new PDO('mysql:host=localhost;dbname=recipe_db', 'root', '');
+        $stmt = $db->prepare('SELECT * FROM Post WHERE UserID = :userid');
+        $stmt->execute([':userid' => $_SESSION['userid']]);
+        $posts = $stmt->fetchAll();
+        foreach ($posts as $post) {
+            echo '<li>';
+            echo '<a href="View_Post.php?postid=' . $post['PostID'] . '">';
+            echo '<img src="' . $post['Img'] . '" alt="Image voor recept" width="100" height="100"';
+            echo '<p class="post-title">' . $post['Title'] . '</p>';
+            echo '</a>';
+            echo '</li>';
+        }
+        ?>
+    </ul>
     <a id="logout-button" href="logout.php">Logout</a>
 </body>
 </html>
